dd98aa8ece1cbd5b22f007bbf8eac02b540569b0
Alexis <Alexis@LAPTOP-74MORNRF>
2018-12-15 20:45:52 +0000

    Kata5P1 version 2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,9 +1,10 @@
 package kata5p1;
 
+import static kata5p1.SelectApp.createNewTable;
+
 public class Kata5P1 {
     public static void main(String[] args) {
-        SelectApp app = new SelectApp();
-        app.selectAll();
+        createNewTable();
     }
 }
 
--- src/kata5p1/SelectApp.java
+++ src/kata5p1/SelectApp.java
@@ -33,4 +33,20 @@ public class SelectApp {
             System.out.println(e.getMessage());
         }
     }
-}
+    
+    public static void createNewTable() {
+        String url = "jdbc:sqlite:mail.db";
+        String sql = "CREATE TABLE IF NOT EXISTS direcc_email (\n"
+        + " id integer PRIMARY KEY AUTOINCREMENT,\n"
+        + " direccion text NOT NULL);";
+        
+        try (Connection conn = DriverManager.getConnection(url);
+                Statement stmt = conn.createStatement()){
+            stmt.execute(sql);
+            System.out.println("Tabla creada");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+} 
+        }
+    }
+
